7b7059413742db7ff276f52f97d29a35c29c5ad3
UpendraDudekonda <2124upendra@gmail.com>
2026-07-12 11:36:38 +0530

    gateway routes,cors,filter

---
+++ gateway-service/src/main/java/com/upendra/portfolio/gateway/config/CorsConfig.java
@@ -0,0 +1,38 @@
+package com.upendra.portfolio.gateway.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+
+
+
+@Configuration
+public class CorsConfig {
+
+	@Bean
+    CorsConfiguration corsConfiguration() {
+
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "http://localhost:3000"
+        ));
+
+        configuration.setAllowedMethods(List.of(
+                "GET",
+                "POST",
+                "PUT",
+                "DELETE",
+                "PATCH",
+                "OPTIONS"
+        ));
+
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        return configuration;
+    }
+}
---
+++ gateway-service/src/main/java/com/upendra/portfolio/gateway/filter/RequestLoggingFilter.java
@@ -0,0 +1,40 @@
+package com.upendra.portfolio.gateway.filter;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class RequestLoggingFilter extends OncePerRequestFilter{
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        long start = System.currentTimeMillis();
+
+        filterChain.doFilter(request, response);
+
+        long end = System.currentTimeMillis();
+
+        System.out.println(
+                request.getMethod() +
+                " " +
+                request.getRequestURI() +
+                " -> " +
+                response.getStatus() +
+                " (" +
+                (end - start) +
+                " ms)"
+        );
+    }
+
+}
